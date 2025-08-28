99efdd0fecfb00d505368ffc08cedc37011ea079
Tejas Nikhar <tejastn10@gmail.com>
2025-08-28 08:30:00 +0530

    chore: replace eslint and prettier with biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from "vite";
-
-import { resolve } from "path";
-
-import dts from "vite-plugin-dts";
+import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
